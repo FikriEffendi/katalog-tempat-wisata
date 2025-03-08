b6adcc10af0e393cc1c5bb81c6df853ba0fff65a
aerochops <150325960+aerochops@users.noreply.github.com>
2025-03-08 15:18:07 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/week_1_katalog_tempat_wisata/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week_1_katalog_tempat_wisata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contacts',
+      theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 16.0),
+            child: Text("Surabaya Submarine Monument"),
+          ),
+        ],
+      ),
+    );
+  }
+}
